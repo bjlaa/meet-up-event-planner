c7cd0a8ebde6ce98dc86a3e13175188bcaead277
Benjamin <benjamin.j.arias@gmail.com>
2016-03-01 17:20:21 +0100

    feat: Created components and set base for building app

---
+++ components/CreateUserAccount.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class CreateUserAccount extends React.Component {
+	render() {
+		return(<div></div>)
+	}
+}
+
+export default CreateUserAccount;
--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
 import { createHistory } from 'history';
 
-class App extends React.Component {
-	render() {
-		return(
-			<div>Mes glawies</div>
-		)
-	}
-}
+/*
+	Components
+*/
+import HomePage from '../components/HomePage.js';
+import CreateEvent from '../components/createEvent.js';
+import CreateUserAccount from '../components/createUserAccount.js';
+import UserHomePage from '../components/UserHomePage.js';
+import UserLogin from '../components/UserLogin.js';
 
 var routes = (
 	<Router history={createHistory()}>
-		<Route path="/" component={App} />
+		<Route path="/" component={Homepage} />
+		<Route path="/login" component={UserLogin} />
+		<Route path="/homepage" component={UserHomePage} />
+		<Route path="/createaccount" component={CreateUserAccount} />
+		<Route path="/createevent" component={CreateEvent} />
 	</Router>
 )
 
